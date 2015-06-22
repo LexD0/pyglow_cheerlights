96c46afbcc628c08368dd12563237bcde341dcd2
Lex <tseril@gmail.com>
2015-06-22 17:05:02 -0400

    first commit

---
+++ cheerlights.py
@@ -0,0 +1,45 @@
+import string
+from PyGlow import PyGlow
+from time import sleep
+import urllib2
+import json
+
+pyglow=PyGlow()
+f = urllib2.urlopen('http://api.thingspeak.com/channels/1417/field/1/last.json')
+json_string = f.read()
+parsed_json = json.loads(json_string)
+
+cheer = parsed_json["field1"]
+print "Current color is %s" % (cheer)
+f.close()
+pyglow.all(0)
+   
+if cheer == "red":
+    pyglow.color("red", 255)
+elif cheer == "green":
+    pyglow.color("green", 255)
+elif cheer == "blue":
+    pyglow.color("blue", 255)
+elif cheer == "cyan":
+    pyglow.color("blue", 255)
+    pyglow.color("green", 255)
+elif cheer == "white":
+    pyglow.color("white", 255)
+elif cheer == "warmwhite" or cheer == "oldlace":
+    pyglow.color("red", 253)
+    pyglow.color("green", 245)
+    pyglow.color("blue", 230)
+elif cheer == "purple":
+    pyglow.color("red", 128)
+    pyglow.color("blue", 128)
+elif cheer == "magenta":
+    pyglow.color("red", 255)
+    pyglow.color("blue", 255)
+elif cheer == "yellow":
+    pyglow.color("yellow", 255)
+elif cheer == "orange":
+    pyglow.color("orange", 255)
+elif cheer == "pink":
+    pyglow.color("red", 255)
+    pyglow.color("white", 255)
+        
